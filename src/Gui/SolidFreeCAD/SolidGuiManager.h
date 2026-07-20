ed93b5e9bd45e44d59c149d482ca00ca4bac24bf
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-20 15:10:46 -0400

    Track light CAD palette and model manager styling

--- src/Gui/SolidFreeCAD/SolidGuiManager.h
+++ src/Gui/SolidFreeCAD/SolidGuiManager.h
@@ -39,6 +39,8 @@ protected:
 private:
     void enforceSolidChrome();
     void ensureModelManager();
+    void applyVisualPalette();
+    void restoreVisualPalette();
     void hideClassicChrome();
     void hideBottomUtilityDocks();
     void restoreModelManager();
@@ -53,9 +55,24 @@ private:
     QList<QPointer<QDockWidget>> hiddenDocks_;
     QPointer<QDockWidget> modelManagerDock_;
     QString modelManagerOriginalTitle_;
+    QString modelManagerOriginalStyle_;
     int modelManagerOriginalMinimumWidth_ = 0;
     bool modelManagerWasVisible_ = false;
     bool modelManagerAddedBySolid_ = false;
+
+    bool visualPaletteApplied_ = false;
+    bool previousGradient_ = true;
+    bool previousRadialGradient_ = false;
+    bool previousSimpleBackground_ = false;
+    bool previousUseMidColor_ = false;
+    unsigned long previousBackgroundColor_ = 0;
+    unsigned long previousBackgroundColor2_ = 0;
+    unsigned long previousBackgroundColor3_ = 0;
+    unsigned long previousBackgroundColor4_ = 0;
+    unsigned long previousDefaultShapeColor_ = 0;
+    unsigned long previousSelectionColor_ = 0;
+    unsigned long previousHighlightColor_ = 0;
+
     boost::signals2::scoped_connection commandChangedConnection_;
 };
 
