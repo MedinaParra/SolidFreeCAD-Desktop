4490b003ddcd2d36f27df442928fc08f1c8f18ab
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-21 14:32:02 -0400

    Add safe workspace polish controller header

---
+++ src/Gui/SolidFreeCAD/SolidWorkspacePolish.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <QPointer>
+
+#include <QObject>
+
+class QLabel;
+class QToolBar;
+
+namespace Gui
+{
+class MainWindow;
+}
+
+namespace SolidFreeCAD
+{
+
+class SolidWorkspacePolish final : public QObject
+{
+public:
+    explicit SolidWorkspacePolish(QObject* parent = nullptr);
+    ~SolidWorkspacePolish() override;
+
+    bool install(Gui::MainWindow* mainWindow);
+    void uninstall();
+    bool isInstalled() const;
+
+private:
+    void apply();
+    void polishRibbon(QToolBar* ribbon);
+    void polishSidePanels();
+    void ensureContextBadge(QWidget* shell);
+    void updateDocumentTitle(QWidget* shell);
+    void stylePrimaryCommands(QToolBar* ribbon);
+
+    Gui::MainWindow* mainWindow_ = nullptr;
+    QPointer<QLabel> contextBadge_;
+    QPointer<QLabel> documentTitle_;
+    bool installed_ = false;
+};
+
+}  // namespace SolidFreeCAD
