596af5f2fdad68beef96f204f6f295130be24c67
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-24 09:00:23 -0400

    Add task overlay guard interface

---
+++ src/Gui/SolidFreeCAD/SolidTaskOverlayGuard.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <QObject>
+#include <QPointer>
+
+class QTimer;
+
+namespace Gui
+{
+class MainWindow;
+}
+
+namespace SolidFreeCAD
+{
+
+class SolidTaskOverlayGuard final : public QObject
+{
+public:
+    explicit SolidTaskOverlayGuard(QObject* parent = nullptr);
+    ~SolidTaskOverlayGuard() override;
+
+    bool install(Gui::MainWindow* mainWindow);
+    void uninstall();
+    bool isInstalled() const;
+
+private:
+    void refresh();
+    bool activeSketch() const;
+    void synchronizeContextBadges(bool editing);
+    void normalizeTaskDocks(bool editing);
+
+    Gui::MainWindow* mainWindow_ = nullptr;
+    QPointer<QTimer> refreshTimer_;
+    bool installed_ = false;
+};
+
+}  // namespace SolidFreeCAD
