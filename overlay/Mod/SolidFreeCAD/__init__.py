b0ce249d3b6d7c0d57595c6474c46e4adc781cd3
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-08-02 20:41:12 -0400

    Make SolidFreeCAD a regular Python package

---
+++ overlay/Mod/SolidFreeCAD/__init__.py
@@ -0,0 +1,7 @@
+"""SolidFreeCAD desktop workbench package.
+
+Keeping an explicit package initializer gives FreeCAD's InitGui loader a stable
+module path on Windows instead of relying on namespace-package behaviour.
+"""
+
+__version__ = "0.1.0-alpha.6"
