8527180ba3c686addea1b32f4ecc4f5bebf00a3f
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-08-03 06:20:35 -0400

    Activate alpha10 native binding workspace

--- overlay/Mod/SolidFreeCAD/Workbench.py
+++ overlay/Mod/SolidFreeCAD/Workbench.py
@@ -18,7 +18,7 @@ def _available(command_names):
 
 def _apply_window_branding():
     main_window = Gui.getMainWindow()
-    main_window.setWindowTitle("SolidFreeCAD Desktop alpha.9 · Feature Manager Prototype")
+    main_window.setWindowTitle("SolidFreeCAD Desktop alpha.10 · Native Bindings Prototype")
     main_window.setWindowIcon(QtGui.QIcon(_ICON_PATH))
     status_label = main_window.findChild(QtWidgets.QLabel, "SolidFreeCADStatusBrand")
     if status_label is None:
@@ -58,7 +58,7 @@ class SolidFreeCADWorkbench(Gui.Workbench):
             self.appendMenu("SolidFreeCAD", workflow_commands)
 
     def Activated(self):
-        from SolidFreeCAD.FeatureManagerWorkspaceCompat import show_workspace
+        from SolidFreeCAD.FeatureBindingWorkspaceCompat import show_workspace
         from SolidFreeCAD.CommandBridge import patch_command_manager
 
         _apply_window_branding()
@@ -69,7 +69,7 @@ class SolidFreeCADWorkbench(Gui.Workbench):
         patch_command_manager()
 
     def Deactivated(self):
-        from SolidFreeCAD.FeatureManagerWorkspaceCompat import hide_workspace
+        from SolidFreeCAD.FeatureBindingWorkspaceCompat import hide_workspace
 
         hide_workspace()
 
